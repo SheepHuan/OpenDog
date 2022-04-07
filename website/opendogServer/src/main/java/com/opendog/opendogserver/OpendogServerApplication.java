9cfaa3c5e6aef315bdcab541ae0eb53f503dc46d
SheepHuan <justno@foxmail.com>
2022-04-07 20:01:40 +0800

    fix: 修复Application启动失败

--- website/opendogServer/src/main/java/com/opendog/opendogserver/OpendogServerApplication.java
+++ website/opendogServer/src/main/java/com/opendog/opendogserver/OpendogServerApplication.java
@@ -1,11 +1,13 @@
 package com.opendog.opendogserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
+@MapperScan("com.opendog.opendogserver.mapper")
 public class OpendogServerApplication {
 
     public static void main(String[] args) {
