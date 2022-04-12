1f6e1eceaf203dbed0b1f9d2e6d66b932aa21c53
SheepHuan <justno@foxmail.com>
2022-04-12 11:14:45 +0800

    Revert "feat: 新增返回状态"
    This reverts commit 7a8eb4f458236d613842ec663c0d393018466958.

--- website/opendogServer/src/main/java/com/opendog/opendogserver/utils/RetState.java
+++ website/opendogServer/src/main/java/com/opendog/opendogserver/utils/RetState.java
@@ -2,12 +2,8 @@ package com.opendog.opendogserver.utils;
 
 public enum RetState {
 
-    SUCCESS(1000,"成功"),
-    ERROR(2000,"错误"),
-    UNAUTHORIZED(2001,"未授权"),
-    BADHEADER(2002,"错误的请求头"),
-    BADPARAMS(2003,"错误的请求参数"),
-    DATABASEERROR(2004,"数据库操作异常");
+    SUCCESS(1000,"SUCCESS"),
+    ERROR(2000,"ERROR");
 
     private int state;
     private String description;
