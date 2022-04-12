b8552d957affaa577a8329547dabe3435a4ef7c3
SheepHuan <justno@foxmail.com>
2022-04-12 22:03:18 +0800

    fix: 修复依赖

--- website/opendogServer/src/main/java/com/opendog/opendogserver/OpendogServerApplication.java
+++ website/opendogServer/src/main/java/com/opendog/opendogserver/OpendogServerApplication.java
@@ -1,10 +1,10 @@
 package com.opendog.opendogserver;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication()
 @MapperScan("com.opendog.opendogserver.mapper")
